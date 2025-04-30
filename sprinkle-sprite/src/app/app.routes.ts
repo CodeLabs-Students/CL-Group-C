be294de1d7d0ee55656d0c945a057c7314f0f4bd
BluecollarGiant <Mr.isaiahratliff@gmail.com>
2025-04-30 10:40:15 -0500

    Revert "Fix casing: Layout → layout"
    This reverts commit 4c98c8b3d83e965d099623886f9be1464d1c27c7.

--- sprinkle-sprite/src/app/app.routes.ts
+++ sprinkle-sprite/src/app/app.routes.ts
@@ -1,11 +1,3 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './layout/main/mainbody/pov/home/home.component';
 
-
-
-export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  // other routes your teammate is working on
-];
-
-
+export const routes: Routes = [];
