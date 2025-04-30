b24794a0dd58eae02c5e55da6a99a5cddc77f76e
Matthew Curtis <160465720+mdcurt@users.noreply.github.com>
2025-04-30 10:37:54 -0500

    Add routing configuration for Home, Account, Checkout, and Menu components

--- sprinkle-sprite/src/app/app.routes.ts
+++ sprinkle-sprite/src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './layout/main/mainbody/pov/home/home.component';
+import { AccountComponent } from './layout/main/mainbody/pov/account/account.component';
+import { CheckoutComponent } from './layout/main/mainbody/pov/checkout/checkout.component';
+import { MenuComponent } from './layout/main/mainbody/pov/menu/menu.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+
+  },
+  {
+    path: 'account',
+    component: AccountComponent
+
+  },
+  {
+    path: 'checkout',
+    component: CheckoutComponent
+
+  },
+  {
+    path: 'menu',
+    component: MenuComponent
+  }
+];
