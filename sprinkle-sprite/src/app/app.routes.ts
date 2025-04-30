4c98c8b3d83e965d099623886f9be1464d1c27c7
BluecollarGiant <Mr.isaiahratliff@gmail.com>
2025-04-30 10:06:14 -0500

    Fix casing: Layout → layout

--- sprinkle-sprite/src/app/app.routes.ts
+++ sprinkle-sprite/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './layout/main/mainbody/pov/home/home.component';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  // other routes your teammate is working on
+];
+
+
