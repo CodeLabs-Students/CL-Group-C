73b18e06cbb11f0ca4cec6fef8761b1d25242100
NimCloud90 <shayleefarris@gmail.com>
2025-05-01 19:53:29 -0500

    we figured out how to get barebones functionality, we're so back

--- sprinkle-sprite/src/app/layout/main/mainbody/pov/menu/menu.component.ts
+++ sprinkle-sprite/src/app/layout/main/mainbody/pov/menu/menu.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+
+  export interface Card {
+    id: number;
+    title: string;
+    description: string;
+  }
 
 @Component({
   selector: 'app-menu',
@@ -9,8 +16,35 @@ import { Component } from '@angular/core';
 
 export class MenuComponent {
  
-  Flavors: string = '';
-  id!: number;
+
+  cards: Card[] = [];
+  currentPage = 1;
+  cardsPerPage = 6;
+
+  get paginatedCards() {
+    const start = (this.currentPage - 1) * this.cardsPerPage;
+    return this.cards.slice(start, start + this.cardsPerPage);
+  }
+
+  ngOnInit() {
+    this.cards = Array.from({ length: 20 }, (_, i) => ({
+      id: i + 1,
+      title: `Card ${i + 1}`,
+      description: `Description fror card ${i + 1}`,
+    }));
+  }
+
+  nextPage() {
+    if (this.currentPage < Math.ceil(this.cards.length / this.cardsPerPage)) {
+      this.currentPage++;
+    }
+  }
+
+  prevPage() {
+    if(this.currentPage > 1) {
+      this.currentPage--;
+    }
+  }
 
   count: number = 0;
 
