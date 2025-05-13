576309a63d77bfb503ea00af5a55f89755b9fd24
BluecollarGiant <Mr.isaiahratliff@gmail.com>
2025-05-13 12:12:42 -0500

    set up routing and button click rewards pov confirmed successful

--- sprinkle-sprite/src/app/app.routes.ts
+++ sprinkle-sprite/src/app/app.routes.ts
@@ -18,15 +18,10 @@ export const routes: Routes = [
 
     ],
   },
-  {
-    path: 'login',
-    loadComponent: () =>
-      import('./login/login.component').then((m) => m.LoginComponent),
+  { path: 'login', loadComponent: () => import('./login/login.component').then((m) => m.LoginComponent),
   },
-  {
-    path: 'create-account',
-    loadComponent: () =>
-      import('./create-user/create-user.component').then(m => m.CreateUserComponent)
+  { path: 'create-account', loadComponent: () => import('./create-user/create-user.component').then(m => m.CreateUserComponent)
   },
+  { path: 'vip-rewards', loadComponent: () => import('./rewards/vip-rewards.component').then(m => m.VipRewardsComponent) },
 
 ];
