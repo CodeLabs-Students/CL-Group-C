7c8ef18b1fb709bb21f43862e4c75485b979c070
Matthew Curtis <160465720+mdcurt@users.noreply.github.com>
2025-05-15 01:32:31 -0500

    Reverting
    Reverting merge from earlier mishap.

--- sprinkle-sprite/src/app/backend/inventory.service.ts
+++ sprinkle-sprite/src/app/backend/inventory.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Signal } from '@angular/core';
 import { Firestore, collection, collectionData } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 import { inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 export interface Flavor {
   name: string;
@@ -17,8 +18,16 @@ export class InventoryService {
   private firestore: Firestore = inject(Firestore);
 
 
+  private readonly flavors$ = collectionData(
+    collection(this.firestore, 'inventory'),
+    { idField: 'id' }
+  ) as Observable<Flavor[]>;
+
+
+  readonly flavors: Signal<Flavor[]> = toSignal(this.flavors$, {
+  initialValue: []
+});
   getFlavors(): Observable<Flavor[]> {
-    const flavorsRef = collection(this.firestore, 'inventory'); // 'inventory' = your Firestore collection name
-    return collectionData(flavorsRef, { idField: 'id' }) as Observable<Flavor[]>;
-  }
+  return this.flavors$;
+}
 }
