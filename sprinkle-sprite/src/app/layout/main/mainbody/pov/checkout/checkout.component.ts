156904851a411aa574afe1678e5efa5eb3c8f268
NimCloud90 <shayleefarris@gmail.com>
2025-05-16 21:11:42 -0500

    I am *so close* to getting this worked out i can taste it

--- sprinkle-sprite/src/app/layout/main/mainbody/pov/checkout/checkout.component.ts
+++ sprinkle-sprite/src/app/layout/main/mainbody/pov/checkout/checkout.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { InventoryService } from '../../../../../backend/inventory.service';
+import { CardDataService, Card } from '../../../../../sharedservices/card-data.service';
 
 @Component({
   selector: 'app-checkout',
@@ -7,5 +9,16 @@ import { Component } from '@angular/core';
   styleUrl: './checkout.component.css'
 })
 export class CheckoutComponent {
+  seasonalItems: any[] = [];
+card: any;
+currentPage = 1;
+cardsPerPage = 8;
 
+  constructor(private inventoryService: InventoryService) {}
+
+  ngOnInit(): void {
+    this.inventoryService.getFlavors().subscribe((items) => {
+      this.seasonalItems = items.filter(item => item.rarity === 'Seasonal');
+    });
+  }
 }
